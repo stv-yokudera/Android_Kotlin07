2b32375ce17a4bd7004dc6bfafd0201c82578617
OkuderaYuki <>
2018-04-15 23:29:09 +0900

    [Fix] 不要なimportを削除

--- Android_Kotlin07/app/src/main/java/com/example/yuki/android_kotlin07/Models/Math.kt
+++ Android_Kotlin07/app/src/main/java/com/example/yuki/android_kotlin07/Models/Math.kt
@@ -1,6 +1,5 @@
 package com.example.yuki.android_kotlin07.Models
 
-import android.util.Log
 import com.example.yuki.android_kotlin07.Activities.MainActivity
 
 class Math: Result by MainActivity() {
--- Android_Kotlin07/app/src/main/java/com/example/yuki/android_kotlin07/Models/Result.kt
+++ Android_Kotlin07/app/src/main/java/com/example/yuki/android_kotlin07/Models/Result.kt
@@ -1,7 +1,5 @@
 package com.example.yuki.android_kotlin07.Models
 
-import android.util.Log
-
 interface Result {
     fun result(resultValue: Int)
 }
